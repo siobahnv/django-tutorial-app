2bb69be7fc6ca5d2bc5eda9038f0e62daf25da29
siobahnv <siobahnv@gmail.com>
2019-03-02 01:47:28 -0800

    Part 4 Django tutorial.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,11 +17,20 @@ from django.contrib import admin
 from django.urls import include, path
 from polls import views
 
+# urlpatterns = [
+#     path('', views.IndexView.as_view(), name='index'),
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+#     path('polls/', include('polls.urls')),
+#     path('admin/', admin.site.urls),
+# ]
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('polls/<int:question_id>/', views.detail, name='detail'),
-    path('polls/<int:question_id>/results/', views.results, name='results'),
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
